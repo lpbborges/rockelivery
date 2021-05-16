ec30d95d69bd21d1c580fb63032ca6f343e4ef92
LP <luizpaulobborges@gmail.com>
2021-05-16 14:47:16 -0300

    testing users view

---
+++ test/rockelivery_web/views/users_view_test.exs
@@ -0,0 +1,31 @@
+defmodule RockeliveryWeb.UsersViewTest do
+  use RockeliveryWeb.ConnCase, async: true
+
+  import Phoenix.View
+  import Rockelivery.Factory
+
+  alias RockeliveryWeb.UsersView
+
+  test "renders create.json" do
+    user = build(:user)
+
+    response = render(UsersView, "create.json", user: user)
+
+    assert %{
+             message: "User created!",
+             user: %Rockelivery.User{
+               address: "Nowhere street",
+               age: 27,
+               cep: "12345678",
+               cpf: "12345678900",
+               email: "johndoe@rockelivery.com",
+               id: "98063718-b42d-46f1-a1fe-1d8e1c0c936a",
+               inserted_at: nil,
+               name: "John Doe",
+               password: "123456",
+               password_hash: nil,
+               updated_at: nil
+             }
+           } = response
+  end
+end
