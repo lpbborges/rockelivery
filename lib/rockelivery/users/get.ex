a99200ce6119af3a82dbab6a1b6d5b1123438234
LP <luizpaulobborges@gmail.com>
2021-05-16 11:45:43 -0300

    refactoring uuid checker with plug

--- lib/rockelivery/users/get.ex
+++ lib/rockelivery/users/get.ex
@@ -1,25 +1,7 @@
 defmodule Rockelivery.Users.Get do
-  alias Ecto.UUID
   alias Rockelivery.{Error, Repo, User}
 
-  # def by_id2(id) do
-  #   with {:ok, uuid} <- UUID.cast(id),
-  #        %User{} = user <- Repo.get(User, uuid) do
-  #     {:ok, user}
-  #   else
-  #     :error -> {:error, %{status: :bad_request, result: "Invalid id format"}}
-  #     nil -> {:error, %{status: :not_found, result: "User not found."}}
-  #   end
-  # end
-
   def by_id(id) do
-    case UUID.cast(id) do
-      :error -> {:error, Error.build_id_format_error()}
-      {:ok, uuid} -> get(uuid)
-    end
-  end
-
-  defp get(id) do
     case Repo.get(User, id) do
       nil -> {:error, Error.build_user_not_found_error()}
       user -> {:ok, user}
