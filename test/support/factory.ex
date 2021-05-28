6df00002fbef23e2d5afc6ac6b0b6085fcc3bd52
LP <luizpaulobborges@gmail.com>
2021-05-27 23:25:01 -0300

    fix and mock tests

--- test/support/factory.ex
+++ test/support/factory.ex
@@ -5,13 +5,13 @@ defmodule Rockelivery.Factory do
 
   def user_params_factory do
     %{
-      address: "Nowhere street",
-      age: 27,
-      cep: "12345678",
-      cpf: "12345678900",
-      email: "johndoe@rockelivery.com",
-      name: "John Doe",
-      password: "123456"
+      "address" => "Nowhere street",
+      "age" => 27,
+      "cep" => "12345678",
+      "cpf" => "12345678900",
+      "email" => "johndoe@rockelivery.com",
+      "name" => "John Doe",
+      "password" => "123456"
     }
   end
 
@@ -25,6 +25,21 @@ defmodule Rockelivery.Factory do
       name: "John Doe",
       password: "123456",
       id: "98063718-b42d-46f1-a1fe-1d8e1c0c936a"
+    }
+  end
+
+  def cep_info_factory do
+    %{
+      "bairro" => "Sé",
+      "cep" => "01001-000",
+      "complemento" => "lado ímpar",
+      "ddd" => "11",
+      "gia" => "1004",
+      "ibge" => "3550308",
+      "localidade" => "São Paulo",
+      "logradouro" => "Praça da Sé",
+      "siafi" => "7107",
+      "uf" => "SP"
     }
   end
 end
