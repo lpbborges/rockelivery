5281a9757d2c5434cef5d042ae8d2e19f1f0ccf4
LP <luizpaulobborges@gmail.com>
2021-09-05 05:31:50 -0300

    feat: report generation

---
+++ lib/rockelivery/orders/report_runner.ex
@@ -0,0 +1,33 @@
+defmodule Rockelivery.Orders.ReportRunner do
+  use GenServer
+
+  require Logger
+
+  alias Rockelivery.Orders.Report
+
+  def start_link(_initial_state) do
+    GenServer.start_link(__MODULE__, %{})
+  end
+
+  @impl true
+  def init(state) do
+    Logger.info("Report runner started")
+    schedule_report_generation()
+
+    {:ok, state}
+  end
+
+  @impl true
+  def handle_info(_message, state) do
+    Logger.info("Generatin report...")
+
+    Report.create()
+    schedule_report_generation()
+
+    {:noreply, state}
+  end
+
+  def schedule_report_generation do
+    Process.send_after(self(), :generate, 1000 * 10)
+  end
+end
