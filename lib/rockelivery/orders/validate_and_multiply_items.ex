2c7b7dc12d33ea8f1750d88a849fd0410a371b7a
LP <luizpaulobborges@gmail.com>
2021-09-05 06:02:11 -0300

    set formatting and credo validations

--- lib/rockelivery/orders/validate_and_multiply_items.ex
+++ lib/rockelivery/orders/validate_and_multiply_items.ex
@@ -11,10 +11,11 @@ defmodule Rockelivery.Orders.ValidateAndMultiplyItems do
   defp multiply_items(true, _items, _items_params), do: {:error, "Invalid Ids"}
 
   defp multiply_items(false, items, items_params) do
-    items = Enum.reduce(items_params, [], fn %{"id" => id, "quantity" => quantity}, acc ->
-      item = Map.get(items, id)
-      acc ++ List.duplicate(item, quantity)
-    end)
+    items =
+      Enum.reduce(items_params, [], fn %{"id" => id, "quantity" => quantity}, acc ->
+        item = Map.get(items, id)
+        acc ++ List.duplicate(item, quantity)
+      end)
 
     {:ok, items}
   end
